506816e26c265a28756468d01f78c37abc9bac75
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-11-15 11:26:55 +0530

    removed firebase keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,5 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCgE1yEGr5ERD4iD9xaF3_OCFAGJ8M39QQ',
-    authDomain: 'a5-b4-fb.firebaseapp.com',
-    databaseURL: 'https://a5-b4-fb.firebaseio.com',
-    projectId: 'a5-b4-fb',
-    storageBucket: '',
-    messagingSenderId: '459303276431'
-  }
+  
 };
